9be3a55559fd7ac4132e15b4419616faf4e6f354
Nikitcev Andrey <65443524+n3xxxt@users.noreply.github.com>
2026-02-27 00:21:21 +0300

    Update Footer.tsx
    Signed-off-by: Nikitcev Andrey <65443524+n3xxxt@users.noreply.github.com>

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,13 +17,11 @@ export default ((opts?: Options) => {
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
-        <p>
+        <ul>
           {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
+            <a href={link}>{text}</a>
           ))}
-        </p>
+        </ul>
       </footer>
     )
   }
